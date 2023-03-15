b564f9c07e7c52cfb09ebc7e532414f7d40518ef
Marco Henrique Cunha Dias <105382154+marcocunha11@users.noreply.github.com>
2023-03-15 19:32:20 -0300

    Add files via upload

---
+++ Atividade_3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double comissao, total;
+
+            //usando a classe salario para criar objeto salario
+            salario s;
+            s = new salario();
+
+            //Recebendo valores funcionario
+            Console.WriteLine("Olá!! Qual seu nome?");
+            s.nome = Console.ReadLine();
+            Console.WriteLine("\n");
+
+            //Recebendo valores salario
+            Console.WriteLine("Qual seu salário atual?");
+            s.embolso = double.Parse(Console.ReadLine());
+            Console.WriteLine("\n");
+            Console.WriteLine("Quantas vendas você realizou?");
+            s.vendas = double.Parse(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            comissao = s.vendas * 1.15;
+            total = s.embolso + s.vendas;
+
+            Console.WriteLine(s.nome + " recebeu o salário de: " + s.embolso.ToString("C"));
+            Console.WriteLine("Comissao das vendas: " + comissao.ToString("C"));
+            Console.WriteLine("Total: " + total.ToString("C"));
+
+        }
+    }
+}
---
+++ Atividade_3/salario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade3
+{
+    internal class salario
+    {
+        public string nome;
+        public double embolso;
+        public double vendas;
+    }
+}
